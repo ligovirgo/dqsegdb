eb021e5f462f314e52b23055d3e4363fa0c343f1
Duncan Macleod <duncan.macleod@ligo.org>
2014-01-12 12:50:09 -0600

    setup.py: generate etc environment scripts
        - writes etc/dqsegdb_user_env.{sh,csh} to set PATH and PYTHONPATH

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
+import sys
 import glob
 import os.path
-from setuptools import setup, find_packages
+from distutils.command import install
+from setuptools import (setup, find_packages)
 
 utils = __import__('utils', fromlist=['version'], level=1)
 
@@ -34,12 +38,78 @@ if os.path.isdir('bin'):
 else:
     scripts = []
 
+# extend install to write environment scripts
+shenv = os.path.join('etc', '%s-user-env.sh' % PACKAGENAME)
+cshenv = os.path.join('etc', '%s-user-env.csh' % PACKAGENAME)
+
+class DQSegDBInstall(install.install):
+    """Extension of setuptools install to write source script for
+    users.
+    """
+    def _get_install_paths(self):
+        """Internal utility to get install and library paths for install.
+        """
+        installpath = self.install_scripts
+        if self.install_purelib == self.install_platlib:
+            pythonpath = self.install_purelib
+        else:
+            pythonpath = os.pathsep.join([self.install_platlib,
+                                          self.install_purelib])
+        return installpath, pythonpath
+
+    def write_env_sh(self, fp=shenv):
+        """Write the shell environment script for DQSegDB.
+
+        Parameters
+        ----------
+        fp : `str`
+            path (relative to install prefix) of output csh file
+        """
+        installpath, pythonpath = self._get_install_paths()
+        with open(fp, 'w') as env:
+            print('#!/bin/sh\n', file=env)
+            print('PATH=%s:${PATH}' % (installpath), file=env)
+            print('export PATH', file=env)
+            print('PYTHONPATH=%s:${PYTHONPATH}' % (pythonpath), file=env)
+            print('export PYTHONPATH', file=env)
+
+    def write_env_csh(self, fp=cshenv):
+        """Write the shell environment script for DQSegDB.
+
+        Parameters
+        ----------
+        fp : `str`
+            path (relative to install prefix) of output csh file
+        """
+        installpath, pythonpath = self._get_install_paths()
+        with open(fp, 'w') as env:
+            print('setenv PATH %s:${PATH}' % (installpath), file=env)
+            print('setenv PYTHONPATH %s:${PYTHONPATH}' % (pythonpath),
+                  file=env)
+
+    def run(self):
+        self.write_env_sh()
+        self.write_env_csh()
+        install.install.run(self)
+        print("\n--------------------------------------------------")
+        print("DQSegDB has been installed.")
+        print("If you are running csh, you can set your environment by "
+              "running:\n")
+        print("source %s\n" % os.path.join(self.install_base, cshenv))
+        print("Otherwise, you can run:\n")
+        print("source %s" % os.path.join(self.install_base, shenv))
+        print("--------------------------------------------------")
+    run.__doc__ = install.install.__doc__
+
+
 setup(name=PACKAGENAME,
+      cmdclass={'install': DQSegDBInstall},
       version=VERSION,
       description=DESCRIPTION,
       packages=packagenames,
       ext_modules=[],
       scripts=scripts,
+      data_files=[('etc', [shenv, cshenv])],
       install_requires=['glue', 'pyRXP'],
       provides=[PACKAGENAME],
       author=AUTHOR,
