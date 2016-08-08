330e82548e28f44f21928e40eb88448a312e0010
Duncan Macleod <duncan.macleod@ligo.org>
2016-08-08 16:59:54 -0500

    travis: first cut at travis configuration
    tests end-to-end build of dqsegdb and dependencies, then runs `--help` for all installed binaries as a first cut at some tests

---
+++ .travis/test-all-bin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Travis CI test runner: test --help for all command-line executables
+# Author: Duncan Macleod <duncan.macleod@ligo.org> (2016)
+
+# loop over all bin/ scripts
+for EXE in bin/*_dqsegdb; do
+    # get file-name as PATH executable
+    EXENAME=`basename ${EXE}`
+    EXEPATH=`which ${EXE}`
+    # execute --help with coverage
+    echo "Testing $EXENAME --help..."
+    coverage run --append --source=dqsegdb ${EXEPATH} --help 1>/dev/null;
+done
