d3efb2a9bc4f08e709012de071e28a78fec7c518
glth <gary.hemming@ego-gw.it>
2019-11-13 16:39:22 +0100

    Switch to correct search arg.

--- web/test/classes/Homepage.php
+++ web/test/classes/Homepage.php
@@ -85,7 +85,7 @@ class Homepage {
 	        $ifo_class = NULL;
 	        // Loop IFO.
 	        foreach($ai['Ifos'] as $k => $ifo) {
-	            if(!in_array($_GET['ifo'], $_SESSION['deselected_ifo'])) {
+	            if(!in_array($ifo, $_SESSION['deselected_ifo'])) {
 	                $ifo_class = ' w3-blue';
 	            }
 	            $this->get_segments_form .= "<div id=\"div_ifo_".$ifo."\" class=\"w3-tag".$ifo_class." w3-hover-grey w3-border w3-round w3-margin-right cursor\" onclick=\"deselect_ifo('".$ifo."')\">".str_replace('_', ' ', $ifo)."</div>";
