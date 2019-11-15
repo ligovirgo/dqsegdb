79eabd2a33be1ae3de7af3c1477dee5e9aaf952a
glth <gary.hemming@ego-gw.it>
2019-11-15 13:59:05 +0100

    Ad additional check on length of DQ Flag.

--- web/test/scripts/dqsegdb.js
+++ web/test/scripts/dqsegdb.js
@@ -199,21 +199,23 @@ function deselect_flag(uri, f, max) {
 function update_flag_versions(max) {
 	// Get currently selected flag.
 	var dq_flag = $("#dq_flag").val();
-	console.log('len: ' + dq_flag.length);
-	// If number of elements in list is less than or equal to 10.
-	if(dq_flag.length <= 10) {
-		// Update version div.
-		$.get("scripts/actions.php?action=update_version_div&dq_flag=" + dq_flag, function(r) {
-			// If result retrieved
-			if(r != 0) {
-				// Show versions container.
-				$('#div_versions').removeClass('w3-hide');
-				// Re-write versions field.
-				$('#div_versions_field').html(r);
-				// Show retrieve segment button.
-				$("#div_get_segments_button").removeClass("w3-hide");
-			}
-		});
+	// If DQ flag lenght exists.
+	if($("#dq_flag").val().length > 0) {
+		// If number of elements in list is less than or equal to 10.
+		if(dq_flag.length <= 10) {
+			// Update version div.
+			$.get("scripts/actions.php?action=update_version_div&dq_flag=" + dq_flag, function(r) {
+				// If result retrieved
+				if(r != 0) {
+					// Show versions container.
+					$('#div_versions').removeClass('w3-hide');
+					// Re-write versions field.
+					$('#div_versions_field').html(r);
+					// Show retrieve segment button.
+					$("#div_get_segments_button").removeClass("w3-hide");
+				}
+			});
+		}
 	}
 	// Otherwise, if number of elements in list is more than 10.
 	else {
