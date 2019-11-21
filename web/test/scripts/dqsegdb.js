66c9965c1590499314847ee69bf412fc86a9e000
glth <gary.hemming@ego-gw.it>
2019-11-21 17:50:36 +0100

    Switch coalesced_json to coalesced.json.

--- web/test/scripts/dqsegdb.js
+++ web/test/scripts/dqsegdb.js
@@ -305,7 +305,7 @@ $('#div_view_segments').ready(function(){
 					console.log(afn);
 					$('#div_view_segments').append('<p class="w3-margin-0 w3-margin-top"><strong>Additional payload<span id="span_additional_payload"> <i class="fas fa-spinner w3-spin"></i> Getting additional payload...</span>:</strong></p><textarea id="div_add_payload" class="w3-container w3-border" style="position:relative;width:100%;height:200px"></textarea>');
 					if(afn.match(/.coalesced.json/g)) {
-						$.getJSON(afn, function(aj) {
+						$.getJSON(afn.replace('coalesced_json', 'coalesced.json'), function(aj) {
 							console.log(aj);
 							$('#span_additional_payload').text('');
 							$('#div_add_payload').html(JSON.stringify(aj));
