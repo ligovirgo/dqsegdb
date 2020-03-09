06e72054c72ae6fa8e6e3b0f97d83fcfdee5d7da
robertbruntz <44660610+robertbruntz@users.noreply.github.com>
2020-03-09 12:01:37 -0400

    Fixed 'sphinx.ext.pngmath' to 'sphinx.ext.imgmath'
    Reference: https://github.com/sphinx-doc/sphinx/issues/6739

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary','sphinxcontrib.programoutput','numpydoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary','sphinxcontrib.programoutput','numpydoc']
 
 # configure autosummary
 autosummary_generate = True
