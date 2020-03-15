7ba41744ed4ee2e9a110efbe6100b604cc1f5cd9
Ryan Fisher <ryan.fisher@ligo.org>
2020-03-15 00:31:32 -0400

    Adding copyright and license to all files in server directory that end in .php or .py or .sh

--- server/db/db_utils/component_interface_data_integrity_test_suite/src/DAO.py
+++ server/db/db_utils/component_interface_data_integrity_test_suite/src/DAO.py
@@ -1,3 +1,28 @@
+# Copyright (C) 2014-2020 Syracuse University, European Gravitational Observatory, and Christopher Newport University.  Written by Ryan Fisher and Gary Hemming. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+
+# This program is free software: you can redistribute it and/or modify
+
+# it under the terms of the GNU Affero General Public License as
+
+# published by the Free Software Foundation, either version 3 of the
+
+# License, or (at your option) any later version.
+
+#
+
+# This program is distributed in the hope that it will be useful,
+
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+# GNU Affero General Public License for more details.
+
+#
+
+# You should have received a copy of the GNU Affero General Public License
+
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 DQSEGDB Regression Test Suite
 Data Access Object class file
