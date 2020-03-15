607344813156c9fcf93d381074a9b4fc9b0f6484
Ryan Fisher <ryan.fisher@ligo.org>
2020-03-15 00:21:05 -0400

    Copyright in test and fixing copyright on one in bin

--- test/cats_test.sh
+++ test/cats_test.sh
@@ -1,3 +1,17 @@
+# Copyright (C) 2014-2020 Syracuse University, European Gravitational Observatory, and Christopher Newport University.  Written by Ryan Fisher and Gary Hemming. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 mv cats_test_output cats_test_output_$(date +"%Y%m%d")
 mkdir cats_test_output
 ligolw_segments_from_cats_dqsegdb --separate-categories --individual-results --segment-url https://segments-s6.ligo.org --veto-file H1L1V1-S6_CBC_HIGHMASS_D_OFFLINE-961545543-0.xml -s 931035615 -e 971654415 -o cats_test_output
--- test/insert_test.sh
+++ test/insert_test.sh
@@ -1,3 +1,17 @@
+# Copyright (C) 2014-2020 Syracuse University, European Gravitational Observatory, and Christopher Newport University.  Written by Ryan Fisher and Gary Hemming. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 echo "Usage:  takes in a suffix to append to the default flag name.  Inserts a flag using this suffix'd name, then queries that flag.  You should see active segments in the output."
 suffix=$1
 ligolw_segment_insert_dqsegdb --segment-url https://segments-dev.ligo.org --ifos H1 --name DCH-TEST_RYAN_${suffix} --version=1 --comment "Testing Segment Insertions for SL7" --explain ""Testing Segment Insertions for SL7"" --segment-file=H1_TEST_2.txt --summary-file=H1_TEST_2_SUMMARY.txt --insert
