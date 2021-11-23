d5f9b83966ff6d9047523aa32195be82c06f94fd
Duncan Macleod <duncan.macleod@ligo.org>
2021-11-23 14:31:14 +0000

    dqsegdb.tests: add minimal pytest suite
    mainly as a placeholder for some real tests

---
+++ dqsegdb/tests/test_urifunctions.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for `dqsegdb.urifunctions`
+"""
+
+from unittest import mock
+
+from .. import urifunctions
+
+
+def test_constructSegmentQueryURLTimeWindow():
+    assert urifunctions.constructSegmentQueryURLTimeWindow(
+        "https",
+        "segments.ligo.org",
+        "L1",
+        "TEST-FLAG",
+        1,
+        "metadata",
+        100,
+        200,
+    ) == (
+        "https://segments.ligo.org"
+        "/dq/L1/TEST-FLAG/1?s=100&e=200&include=metadata"
+    )
+
+
+def test_constructSegmentQueryURL():
+    assert urifunctions.constructSegmentQueryURL(
+        "https",
+        "segments.ligo.org",
+        "L1",
+        "TEST-FLAG",
+        "1",
+        "metadata",
+    ) == (
+        "https://segments.ligo.org"
+        "/dq/L1/TEST-FLAG/1?include=metadata"
+    )
